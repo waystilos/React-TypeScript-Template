39f7188ba89826f2e41184785cc70af363792480
Ardon Bailey <waystylez1@gmail.com>
2018-07-03 18:48:03 -0700

    modify files to current project

--- config/paths.js
+++ config/paths.js
@@ -9,21 +9,21 @@ const url = require('url');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-const envPublicUrl = process.env.PUBLIC_URL;
+// const envPublicUrl = process.env.PUBLIC_URL;
 
-function ensureSlash(path, needsSlash) {
-  const hasSlash = path.endsWith('/');
-  if (hasSlash && !needsSlash) {
-    return path.substr(path, path.length - 1);
-  } else if (!hasSlash && needsSlash) {
-    return `${path}/`;
-  } else {
-    return path;
-  }
-}
+// function ensureSlash(path, needsSlash) {
+//   const hasSlash = path.endsWith('/');
+//   if (hasSlash && !needsSlash) {
+//     return path.substr(path, path.length - 1);
+//   } else if (!hasSlash && needsSlash) {
+//     return `${path}/`;
+//   } else {
+//     return path;
+//   }
+// }
 
-const getPublicUrl = appPackageJson =>
-  envPublicUrl || require(appPackageJson).homepage;
+// const getPublicUrl = appPackageJson =>
+//   envPublicUrl || require(appPackageJson).homepage;
 
 // We use `PUBLIC_URL` environment variable or "homepage" field to infer
 // "public path" at which the app is served.
@@ -31,13 +31,14 @@ const getPublicUrl = appPackageJson =>
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl = envPublicUrl ||
-    (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
-}
+// function getServedPath(appPackageJson) {
+//   const publicUrl = getPublicUrl(appPackageJson);
+//   const servedUrl = envPublicUrl ||
+//     (publicUrl ? url.parse(publicUrl).pathname : '/');
+//   return ensureSlash(servedUrl, true);
+// }
 
+// CHANGING FOR TS
 // config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
@@ -54,5 +55,5 @@ module.exports = {
   appTsProdConfig: resolveApp('tsconfig.prod.json'),
   appTsLint: resolveApp('tslint.json'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
+  servedPath: getServedPath(resolveApp('package.json'))
 };
--- scripts/build.js
+++ scripts/build.js
@@ -30,6 +30,7 @@ const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
 const useYarn = fs.existsSync(paths.yarnLockFile);
 
+
 // These sizes are pretty large. We'll warn for bundles exceeding them.
 const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
